d02f41d0c43f7fcdfa888030b44fc5b8aaebbcae
CamilaRanniele <rannielecamsil@gmail.com>
2022-01-28 17:56:27 -0300

    Requisito 67, 68, 69

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import Header from '../components/Header';
 
 function Explore() {
+  const [redirect, setRedirect] = useState({ canRedirectToExploreFoods: false,
+    canRedirectToExploreDrinks: false });
   const EXPLORE = 'Explore';
+
+  function handleClickFoods() {
+    setRedirect({ ...redirect, canRedirectToExploreFoods: true });
+  }
+
+  function handleClickDrinks() {
+    setRedirect({ ...redirect, canRedirectToExploreDrinks: true });
+  }
+
   return (
     <div>
       <Header title={ EXPLORE } btnSearch={ false } />
+      <button
+        data-testid="explore-foods"
+        type="button"
+        onClick={ handleClickFoods }
+      >
+        Explore Foods
+      </button>
+
+      <button
+        data-testid="explore-drinks"
+        type="button"
+        onClick={ handleClickDrinks }
+      >
+        Explore Drinks
+      </button>
+      {redirect.canRedirectToExploreFoods && <Redirect to="/explore/foods" />}
+      {redirect.canRedirectToExploreDrinks && <Redirect to="/explore/drinks" />}
     </div>
   );
 }
