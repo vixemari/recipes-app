e0a95f7843b1d1df07305079c311abf1f693e9ae
CamilaRanniele <rannielecamsil@gmail.com>
2022-01-28 17:01:25 -0300

    Requisitos 85 e 86 - ao clicar nos botoes redireciona para a pagina de favoritos ou acabados

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,22 +1,51 @@
 import React, { useEffect, useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import Header from '../components/Header';
 
 function Profile() {
   const [emailLocal, setEmailLocal] = useState('');
+  const [redirect, setRedirect] = useState({ canRedirectToFavorite: false,
+    canRedirectToDones: false });
+
   const PROFILE = 'Profile';
+
   useEffect(() => {
     const user = JSON.parse(localStorage.getItem('user'));
     const { email } = user;
     setEmailLocal(email);
   }, []);
 
+  function handleClickFavorites() {
+    setRedirect({ ...redirect, canRedirectToFavorite: true });
+  }
+
+  function handleClickDones() {
+    setRedirect({ ...redirect, canRedirectToDones: true });
+  }
+
   return (
     <div>
       <Header title={ PROFILE } btnSearch={ false } />
       <p data-testid="profile-email">{ emailLocal }</p>
-      <button type="button" data-testid="profile-done-btn">Done Recipes</button>
-      <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ handleClickDones }
+      >
+        Done Recipes
+      </button>
+
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ handleClickFavorites }
+      >
+        Favorite Recipes
+      </button>
+
       <button type="button" data-testid="profile-logout-btn">Logout</button>
+      {redirect.canRedirectToDones && <Redirect to="/done-recipes" />}
+      {redirect.canRedirectToFavorite && <Redirect to="/favorite-recipes" />}
     </div>
   );
 }
