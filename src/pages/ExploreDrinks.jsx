15158f55cca3d31093cb5e711277241571934d5b
CamilaRanniele <rannielecamsil@gmail.com>
2022-01-28 18:44:18 -0300

    Requisitos 70,71,72,73

--- src/pages/ExploreDrinks.jsx
+++ src/pages/ExploreDrinks.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import ButtonsOfExplore from '../components/ButtonsOfExplore';
 
 function ExploreDrinks() {
   const EXPLORE_DRINKS = 'Explore Drinks';
   return (
     <div>
       <Header title={ EXPLORE_DRINKS } btnSearch={ false } />
+      <ButtonsOfExplore btnNationality={ false } />
     </div>
   );
 }
--- src/pages/ExploreFoods.jsx
+++ src/pages/ExploreFoods.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import ButtonsOfExplore from '../components/ButtonsOfExplore';
 
 function ExploreFoods() {
   const EXPLORE_FOODS = 'Explore Foods';
   return (
     <div>
       <Header title={ EXPLORE_FOODS } btnSearch={ false } />
+      <ButtonsOfExplore btnNationality isFood />
     </div>
   );
 }
