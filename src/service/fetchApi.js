41313706a016ec670c0e5f6de3b0d418adcc9935
CamilaRanniele <rannielecamsil@gmail.com>
2022-01-28 15:17:18 -0300

    Criado o context e provider

---
+++ src/service/fetchApi.js
@@ -0,0 +1,12 @@
+// const URL = 'https://swapi-trybe.herokuapp.com/api/planets/';
+
+// const data = () => (
+//   fetch(URL)
+//     .then((response) => (
+//       response
+//         .json()
+//     ))
+//     .then((json) => json)
+// );
+
+// export default data;
