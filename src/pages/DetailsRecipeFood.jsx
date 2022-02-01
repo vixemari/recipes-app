0a10ae7b5ee168cabe67ea13422854ea95d78313
CamilaRanniele <rannielecamsil@gmail.com>
2022-01-31 22:25:32 -0300

    Inicio tela de detalhes

--- src/pages/DetailsRecipeFood.jsx
+++ src/pages/DetailsRecipeFood.jsx
@@ -1,25 +1,63 @@
-import React, { useEffect } from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import Context from '../context/Context';
 
-function DetailsRecipeFood() {
-  useEffect(() => {
-    // const { id } = match.params;
-  }, []);
+function DetailsRecipeFood({ match }) {
+  const { recipes } = useContext(Context);
+  // console.log(recipes);
+  const { id } = match.params;
+  const details = recipes.filter((recipe) => recipe.idMeal === id);
+  if (details[0]) {
+    const entries = Object.entries(details[0]);
+    console.log(entries);
+    // console.log(details[0]);
+    return (
+      <div>
+        <img
+          src={ details[0].strMealThumb }
+          alt="recipeDetails"
+          data-testid="recipe-photo"
+          width="100px"
+        />
+        <h1 data-testid="recipe-title">{ details[0].strMeal }</h1>
+        <button type="button" data-testid="share-btn">Compartilhar</button>
+        <button type="button" data-testid="sfavorite-btn">Favoritar</button>
+        <p data-testid="recipe-category">{ details[0].strCategory }</p>
+        {/* Aqui fica os ingredientes - requisito 33 */}
+        <ul>
+          Ingredients:
+          {entries.map((entrie) => {
+            if (entrie[0].includes('strIngredient') && entrie[1] !== '') {
+              return (
+                <li
+                  data-testid={ `${index}-ingredient-name-and-measure` }
+                >
+                  {`${entrie[1]} - ${entrie[0].includes(`strMeasure[${index + 1}]`)[1]}`}
+                </li>
+              );
+            }
+            return '';
+          })}
+        </ul>
 
-  return (
-    <div>
-      <img src="" alt="recipeDetails" data-testid="recipe-photo" />
-      <h1 data-testid="recipe-title">DETALHES DA RECEITA ....</h1>
-      <button type="button" data-testid="share-btn">Compartilhar</button>
-      <button type="button" data-testid="sfavorite-btn">Favoritar</button>
-      <p data-testid="recipe-category">O texto categoria...</p>
-      {/* Aqui fica os ingredientes - requisito 33 */}
-      <p data-testid="instructions">Instruções</p>
-      {/* Aqui fica o video do you tube requisito 33 */}
-      {/* Aqui fica o card de receitas recomendandas requisito 33 */}
-      <button type="button" data-testid="start-recipe-btn">Iniciar Receita</button>
-    </div>
-  );
+        <p data-testid="instructions">{ details[0].strInstructions }</p>
+        {/* Aqui fica o video do you tube requisito 33 */}
+        <iframe
+          width="200"
+          src={ details[0].strYoutube.replace('watch?v=', 'embed/') }
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer;
+          autoplay; clipboard-write;
+          encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+        {/* Aqui fica o card de receitas recomendandas requisito 33 */}
+        <button type="button" data-testid="start-recipe-btn">Iniciar Receita</button>
+      </div>
+    );
+  }
+  return (<p>Carregando</p>);
 }
 
 DetailsRecipeFood.propTypes = {
--- src/routes/index.js
+++ src/routes/index.js
@@ -12,7 +12,7 @@ import ExploreNationalities from '../pages/ExploreNationalities';
 import Profile from '../pages/Profile';
 import DoneRecipes from '../pages/DoneRecipes';
 import FavoriteRecipes from '../pages/FavoriteRecipes';
-// import DetailsRecipeFood from '../pages/DetailsRecipeFood';
+import DetailsRecipeFood from '../pages/DetailsRecipeFood';
 // import DetailsRecipeDrink from '../pages/DetailsRecipeDrink';
 
 function Routes() {
@@ -42,8 +42,8 @@ function Routes() {
       <Route exact path="/profile" component={ Profile } />
       <Route exact path="/done-recipes" component={ DoneRecipes } />
       <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
-      {/* <Route exact path={ `/foods/${idDaReceita}` } component={ DetailsRecipeFood } />
-      <Route exact path={ `/drinks/${idDaReceita}` }
+      <Route exact path="/foods/:id" component={ DetailsRecipeFood } />
+      {/* <Route exact path={ `/drinks/${idDaReceita}` }
       component={ DetailsRecipeDrink } /> */}
 
       {/*
