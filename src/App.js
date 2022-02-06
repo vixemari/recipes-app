9d65cf20423b99f640e3327bfa2124b05d9aab6a
baky2708 <flavioisaque123@gmail.com>
2022-02-06 09:28:13 -0300

    fix(Merge): fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Routes from './routes';
+import Routes from './routes/index';
 import Provider from './context/Provider';
 
 function App() {
