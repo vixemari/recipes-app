a5aaa7be1272654bd06c6156797ecba0f1e7a920
neguinho <victor_maanoel@Outlook.com>
2022-02-07 09:02:19 -0400

    build(FoodInProgress): mudando o arquivo de local para pages e alterando seu nome

---
+++ src/pages/FoodInProgress.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ContentFood from '../components/recipesInProgress/foodInprogress/ContentFood';
+
+export default function FoodInProgress() {
+  return (
+    <div>
+      <ContentFood />
+    </div>
+  );
+}
